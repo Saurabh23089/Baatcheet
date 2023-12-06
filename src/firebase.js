fec1b2b71400c5db8d11c1e5b6afeeb605911703
Saurabh23089 <saurabha9540@gmail.com>
2023-12-06 12:35:35 +0530

    resolving redirect error to deploy

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,26 @@
-import firebase from 'firebase/app';
-import {initializeApp} from 'firebase/app';
-import {getAuth,signInwithRedirect,signInWithPopup,GoogleAuthProvider} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
-import 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain:process.env.REACT_APP_AUTH_DOMAIN,
-    projectId:process.env.REACT_APP_PROJECT_ID,
-    storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId:process.env.REACT_APP_APP_ID
-  };
-  
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const db=getFirestore(app);
-  const auth=getAuth(app);
-  export {db,auth};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Initialize a provider using the Google auth provider
+const provider = new GoogleAuthProvider();
+
+export { db, auth, provider, signInWithRedirect, signInWithPopup };
+
 
   /*
   // Initialize a provider using this google auth provider
